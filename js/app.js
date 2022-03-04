bbeeba62e3a7cfe26d2694960f2c65b8e79ff8c7
Your Name <you@example.com>
2022-03-04 16:59:03 -0500

    push

--- js/app.js
+++ js/app.js
@@ -26,22 +26,3 @@ $('.right_side_navbar_items_mobile a').click(function () {
     $('.mobile_icon').show()
 })
 
-
-$(window).on("load",function() {
-    $(window).scroll(function () {
-        var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-        $(".fade").each(function () {
-            
-            /* Check the location of each desired element */
-            var objectBottom = $(this).offset().top + $(this).outerHeight();
-
-            /* If the element is completely within bounds of the window, fade it in */
-            if (objectBottom <= windowBottom) { //object comes into view (scrolling down)
-                if ($(this).css("opacity") == 0) { $(this).fadeTo(1000, 1); }
-            } else { //object goes out of view (scrolling up)
-                if ($(this).css("opacity") == 1) { $(this).fadeTo(1000, 0); }
-            }
-        });
-    }).scroll(); //invoke scroll-handler on page-load
-});
-
